bb2b3bd78685cbb55a360df072757964d3a047b0
Chase McLain <chasermclain@gmail.com>
2025-08-21 14:16:37 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let heros = [{
+  Name: 'Paul The Barbarian',
+  Health: 100,
+  Level: 1
+},
+{
+  Name: "Wizard",
+  Health: 100,
+  Level: 1
+}
+]
+
+let boss = [
+  {
+    Name: "Bill",
+    Health: 100,
+    Level: 1
+  }
+]
+
+
+function attackBoss() {
+
+
+}
