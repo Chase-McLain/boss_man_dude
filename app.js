7eec368a5e8d12c7374593e02aaaa18df8c9c7d6
Chase McLain <chasermclain@gmail.com>
2025-08-21 16:12:35 -0600

    boss health bar

--- app.js
+++ app.js
@@ -15,34 +15,81 @@ let heroes = [{
 }
 ]
 
-let boss = [
-  {
-    Name: "Bill",
-    Health: 100,
-    Level: 1
-  }
-]
+let boss =
+{
+  Name: "Bill",
+  Health: 100,
+  Level: 1
+}
+
 
 let wizHealthCounterElm = document.getElementById('wizard-health')
 let paulHealthCounterElm = document.getElementById('paul-health')
 let jimmyHealthCounterElm = document.getElementById('jimmy-health')
 
 function currentHealth() {
+  heroes.forEach((hero) => heroes.Health == hero.Health)
+
 
 }
+
+// ----------BOSS SECTION----------------
+
+
+let bossHealthElm = document.getElementById('boss-health')
+
+
+
+function displayBossHealth() {
+  bossHealthElm.innerHTML = `<p> Health: boss.Health</p>`
+
+}
+
+displayBossHealth()
+
+function bossHealth() {
+  boss.Health -= totalAttack
+  console.log(boss.Health)
+}
+
+let totalAttack = 0
 
 function attackBoss() {
-  let totalAttack = 0
   heroes.forEach((hero) => {
-    totalAttack += hero.Level * 2
+    totalAttack = 0
+    totalAttack += hero.Level * 3
   }
-
-
   )
-  console.log(totalAttack)
-
+  bossHealth()
+  displayBossHealth()
+  return totalAttack
 }
+
+
+
+
+
+
+
+
+
+
+
+
+setInterval(damageHeros, 1000 * 5)
+
+function damageHeros() {
+
+  heroes.forEach((hero) => {
+    hero.Health -= boss.Level * 4
+    console.log(hero.Health)
+  })
+}
+
 
 
 
 let gold = 0
+
+
+
